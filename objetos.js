82c9e43ad9e0febb4b9b7d306a79399daa7acc49
Edison Rivera gutierrez <julianysp44@gmail.com>
2022-11-01 17:27:37 -0500

    actividad

---
+++ objetos.js
@@ -0,0 +1,6 @@
+for (var i=1; i<=1; i++){
+    document.body.innerHTML+="<marquee behavior='alternate' height='580' SCROOLAMOUNT=10 direction='down'><div id='circulo'></marquee>";
+}
+for (var i=1; i<=1; i++){
+    document.body.innerHTML+="<marquee behavior='alternate' height='580' SCROOLAMOUNT=10 direction='right','left'><div id='barra'></marquee>";
+}
